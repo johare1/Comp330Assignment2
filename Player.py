7a63fa6b428630d0fed22ad7bad5bbe267bbe9d3
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-10-13 18:22:26 -0500

    Create Player.py
    starting point for player class

---
+++ Player.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 13 17:52:57 2018
+
+@author: singh
+"""
+from Board import Board
+
+class Player(Board):
+    
+    p1 = ""
+    p2 = ""
+    num_wins1 = 0
+    num_wins2 = 0
+    num_draws = 0
+    
+    def setNames(self):
+        p1 = input('Enter your name player 1: ')
+        p2 = input('Enter your name player 1: ')
+        
+    def addWinner(self):
+        if(Board.win == True and Board.turn%2 == 0):
+            num_wins1 = num_wins1 + 1
+        elif(Board.win == True):
+            num_wins2 = num_wins2 + 1
+        elif(Board.draw == True):
+            num_draws = num_draws + 1
+        
+        
+    
