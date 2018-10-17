82deab3c7e9930b4678c1f6cd2788d3f97f4e4b1
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-10-17 14:33:23 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -10,21 +10,27 @@ class Player(Board):
     
     p1 = ""
     p2 = ""
+    global num_wins1
     num_wins1 = 0
+    global num_wins2
     num_wins2 = 0
+    global num_draws
     num_draws = 0
     
     def setNames(self):
         p1 = input('Enter your name player 1: ')
         p2 = input('Enter your name player 1: ')
         
-    def addWinner(self):
-        if(Board.win == True and Board.turn%2 == 0):
+    def addWins(self):
+        if(Board.win == True and self.turn%2 == 0):
             num_wins1 = num_wins1 + 1
+            print(num_wins1)
         elif(Board.win == True):
             num_wins2 = num_wins2 + 1
         elif(Board.draw == True):
             num_draws = num_draws + 1
+         
+
         
         
     
